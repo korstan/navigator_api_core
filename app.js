a5cf1f6777c91670e2229c3971592de7a0aeaabf
korstan <stas.korda@gmail.com>
2020-11-15 14:19:39 +0300

    add koa cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
 const logger = require('koa-logger')
 const Router = require('@koa/router');
+const cors = require('@koa/cors');
 const ApplicationRouter = require ('./src/api/router.js');
 const config = require('./src/config');
 
 const app = new Koa();
+app.use(cors());
 const rootRouter = new Router();
 
 rootRouter.get('/', async (ctx) => {
